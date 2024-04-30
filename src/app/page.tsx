6024a038fc7aea5c41ba29341412e487a84585a2
seominki <tjalsrl0668@naver.com>
2024-04-30 10:29:35 +0900

    [Design] 초기 화면에 className 제거,  meta title 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import Bill from "@/app/(routes)/bill/page";
+import Payment from "@/app/(routes)/[payment]/page";
 
 export default function Home() {
   return (
-    <div className="bg-neutral-100 min-h-screen flex justify-center px-5">
-      <Bill></Bill>
-    </div>
+    <Payment></Payment>
   );
 }
