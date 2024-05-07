ba975e0bbb7a4ebff1e36fb401028040e9ad1d77
seominki <tjalsrl0668@naver.com>
2024-05-07 16:40:23 +0900

    [Feat] 이미지 호스팅 서비스의 호스트 등록

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/paymint.linq.seoul/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
