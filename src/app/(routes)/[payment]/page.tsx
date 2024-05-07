b87e2a8c6bfabf0e91d74635c72aaf0f1dafdf20
seominki <tjalsrl0668@naver.com>
2024-05-07 16:39:44 +0900

    [Feat] 결제수단 container, 결제앱 component 추가, 결제수단 타입 파일 생성

--- src/app/(routes)/[payment]/page.tsx
+++ src/app/(routes)/[payment]/page.tsx
@@ -5,6 +5,7 @@ import { useRouter, useSearchParams, usePathname } from "next/navigation";
 import Title from "@/components/payment/Title";
 import ShareButton from "@/components/payment/ShareButton";
 import BillInfo from "@/containers/BillInfo";
+import PayTypes from "@/containers/PayTypes";
 
 type LedgerShortUrl = string;
 
@@ -15,7 +16,6 @@ const Payment = () => {
 
   return (
     <div className="bg-neutral-100 min-h-screen flex justify-center px-5">
-      <p>{ledgerShortUrl}</p>
       <div className="max-w-[640px] w-full">
         <div className="flex justify-between items-start self-stretch flex-grow-0 flex-shrink-0 relative pl-2 pt-9 mb-7">
           <Title title={`청구서 \n 납부하기`}></Title>
@@ -30,6 +30,8 @@ const Payment = () => {
           messageTitle="안내메123"
           message="안녕"
         ></BillInfo>
+        <h2 className="px-2 mt-7 mb-3 text-[18px] font-bold">결제수단 선택</h2>
+        <PayTypes></PayTypes>
       </div>
     </div>
   );
---
+++ src/components/payType/PayApp.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { PayAppOptions } from "@/types/payType";
+
+const PayApp = ({payAppName, payAppImage}: PayAppOptions) => {
+  return (
+    <div className="px-2">
+      <Image 
+        src={payAppImage} alt="결제앱 이미지"
+        className="rounded-[14px]"
+        width={60}
+        height={60}
+      ></Image>
+      <p className="text-[10px] font-medium text-center mt-2">{ payAppName }</p>
+    </div>
+  )
+}
+
+export default PayApp;
