eb2152acd742e6a50b19e4b03d7b925b003332f3
seominki <tjalsrl0668@naver.com>
2024-05-30 11:55:46 +0900

    [Feat] 토스 결제 위젯 - props data 추가 및 애니메이션 추가

--- src/components/payment/PaymentWidget.tsx
+++ src/components/payment/PaymentWidget.tsx
@@ -1,40 +1,60 @@
 import { useState, useEffect, useRef } from "react";
 import { fetchPaymentWidget, renderPaymentWidget, handlePaymentRequest } from "@/hooks/paymentRequest";
+import { motion } from 'framer-motion';
 
-const PaymentWidget = () => {
-  const [paymentWidget, setPaymentWidget] = useState<any>(null);
-  const paymentMethodsWidgetRef = useRef(null);
-
-  useEffect(() => {
-    const init = async () => {
-      const fetch = await fetchPaymentWidget();
-      setPaymentWidget(fetch);
-    }
-    init();
-    
-  }, [])
-
-  useEffect(() => {
-    if (paymentWidget == null) {
-      return;
-    }
-
-    renderPaymentWidget(paymentWidget, "10000", paymentMethodsWidgetRef);
-
-  }, [paymentWidget])
-
-  return (
-    <div className="fixed px-5 left-0 top-0 w-full h-full bg-opacity flex items-center justify-center">
-      <section className="w-[640px] rounded-xl overflow-hidden">
-        <div id="payment-widget"/>
-        <div id="agreement"/>
-        <button onClick={() => handlePaymentRequest(paymentWidget)}
-          className="w-full bg-[#3282f6] p-4 text-white"
-        >
-          결제하기
-        </button>
-      </section>
-    </div>
-  )
+interface PaymentWidgetProps {
+  price: number;
+  payRequest: boolean,
+  handleWidget: () => void;
 }
+
+const PaymentWidget = (({ price, payRequest, handleWidget }: PaymentWidgetProps) => {
+    const [paymentWidget, setPaymentWidget] = useState<any>(null);
+    const paymentMethodsWidgetRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const init = async () => {
+        const fetch = await fetchPaymentWidget();
+        setPaymentWidget(fetch);
+      };
+
+      if (payRequest) {
+        init();
+      }
+
+      
+    }, [payRequest]);
+
+    useEffect(() => {
+      if (paymentWidget === null) {
+        return;
+      }
+
+      renderPaymentWidget(paymentWidget, price, paymentMethodsWidgetRef)
+    }, [paymentWidget]);
+
+    return (
+      <div className="fixed px-5 left-0 top-0 w-full h-full bg-opacity ">
+        <motion.div className="px-5 w-full h-full flex items-center justify-center"
+          onClick={handleWidget}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "100%" }}
+          transition={{ duration: 0.3 }}
+        >
+          <section className="w-[640px] rounded-xl overflow-hidden">
+            <div id="payment-widget" />
+            <button
+              onClick={() => handlePaymentRequest(paymentWidget)}
+              className="w-full bg-[#3282f6] p-4 text-white"
+            >
+              결제하기
+            </button>
+          </section>
+        </motion.div>
+      </div>
+    );
+  }
+);
+
 export default PaymentWidget;
