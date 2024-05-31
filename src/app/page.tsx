e6b2ecfef30a6bd00ef354f68751e6f1e1db8b3d
seominki <tjalsrl0668@naver.com>
2024-05-31 18:09:34 +0900

    [Feat] 홈 화면 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
-import Payment from "@/app/(routes)/payment/page";
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import ButtonComponent from "@/components/ButtonComponent";
+import CoffeeLogo from "public/images/coffee-logo.png"
 
-export default function Home() {
+const Home = () => {
+  const startButtonStyles: string  ="absolute bottom-[72px] left-1/2 translate-x-[-50%] w-9/12 h-[60px] rounded-xl text-[18px] text-white bg-[#232526]";
+  const router = useRouter();
+
+  const handleStart = ():void => {
+    router.push("/payment");
+  }
   return (
-    <Payment></Payment>
+    <div className="min-h-screen relative">
+      <section className="w-full py-6 flex items-center justify-center flex-col">
+        <Image 
+          src={CoffeeLogo}
+          alt="커피로고"
+          width={200}
+          height={200}
+        />
+        <h1 className="text-xl mt-6">Welcome to Coffee Bar</h1>
+      </section> 
+      
+      <ButtonComponent
+        classNames={startButtonStyles}
+        buttonText="ORDER"
+        handleClick={handleStart}
+      />
+    </div>
   );
 }
+
+export default Home;
