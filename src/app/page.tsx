18e69416545a4650bf4aa84e0b5d5da12d1005c1
seominki <tjalsrl0668@naver.com>
2024-06-03 15:25:12 +0900

    [Design] 홈 화면 css 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,31 @@
 "use client";
-import Image from "next/image";
+
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import CoffeeLogo from "public/images/coffee-logo.png";
 import ButtonComponent from "@/components/ButtonComponent";
-import CoffeeLogo from "public/images/coffee-logo.png"
 
 const Home = () => {
-  const startButtonStyles: string  ="absolute bottom-[72px] left-1/2 translate-x-[-50%] w-9/12 h-[60px] rounded-xl text-[18px] text-white bg-[#232526]";
   const router = useRouter();
 
-  const handleStart = ():void => {
-    router.push("/payment");
-  }
+  const handleStart = (): void => {
+    router.push("/menu");
+  };
+
   return (
-    <div className="min-h-screen relative">
-      <section className="w-full py-6 flex items-center justify-center flex-col">
-        <Image 
-          src={CoffeeLogo}
-          alt="커피로고"
-          width={200}
-          height={200}
-        />
+    <div className="h-full w-full fixed bg-neutral-100 py-8 flex items-center flex-col justify-between overflow-auto">
+      <section>
+        <Image src={CoffeeLogo} alt="커피로고" width={200} height={200} />
         <h1 className="text-xl mt-6">Welcome to Coffee Bar</h1>
-      </section> 
+      </section>
       
       <ButtonComponent
-        classNames={startButtonStyles}
+        classNames="w-9/12 h-[60px] rounded-xl text-[18px] text-white bg-[#232526]"
         buttonText="ORDER"
         handleClick={handleStart}
       />
     </div>
   );
-}
+};
 
 export default Home;
