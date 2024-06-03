5e6078112f3035f6fa42bdc5b476ffa2de114445
seominki <tjalsrl0668@naver.com>
2024-06-03 16:36:56 +0900

    [Design] 폰트 추가,  버튼 배경색 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@
 
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import localFont from "next/font/local"
 import CoffeeLogo from "public/images/coffee-logo.png";
 import ButtonComponent from "@/components/ButtonComponent";
+
+const eduQLDBeginner = localFont({
+  src: "../../public/fonts/EduQLDBeginner-VariableFont_wght.ttf",
+  display: "swap",
+  weight: "400 700",
+})
 
 const Home = () => {
   const router = useRouter();
@@ -13,14 +20,14 @@ const Home = () => {
   };
 
   return (
-    <div className="h-full w-full fixed bg-neutral-100 py-8 flex items-center flex-col justify-between overflow-auto">
+    <div className="h-full w-full fixed py-8 flex items-center flex-col justify-between overflow-auto">
       <section>
-        <Image src={CoffeeLogo} alt="커피로고" width={200} height={200} />
-        <h1 className="text-xl mt-6">Welcome to Coffee Bar</h1>
+        <Image src={CoffeeLogo} alt="커피로고" width={200} priority={true} style={{ width: 200, height: 'auto' }} />
+        <h1 className={`text-xl mt-6 ${eduQLDBeginner.className}`}>Welcome to Coffee Bar</h1>
       </section>
       
       <ButtonComponent
-        classNames="w-9/12 h-[60px] rounded-xl text-[18px] text-white bg-[#232526]"
+        classNames="w-9/12 h-[60px] rounded-xl text-[18px] text-white bg-brown"
         buttonText="ORDER"
         handleClick={handleStart}
       />
