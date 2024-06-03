031439b0038ca5b13e31162f5229b23dcbb9325a
seominki <tjalsrl0668@naver.com>
2024-06-03 16:32:47 +0900

    [Design] tailwinde 커스텀 컬러 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "font": "#2b2b2b"
+        "beige": "#FED8B1",
+        "brown": "#6F4E37"      
       }
     },
   },
