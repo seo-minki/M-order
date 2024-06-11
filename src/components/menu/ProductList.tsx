d40ef106d1a92d2d86e74f0d93aafe5aad5bdd3e
seominki <tjalsrl0668@naver.com>
2024-06-11 18:05:24 +0900

    [Feat] 상품 카테고리 변경 시 상품 목록 애니메이션 추가

--- src/components/menu/ProductList.tsx
+++ src/components/menu/ProductList.tsx
@@ -1,24 +1,58 @@
 import { ProductOptions } from "@/types/menu";
 import Product from "./Product";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 interface PropsProductList {
-  list : Array<ProductOptions>
+  list: Array<ProductOptions>;
 }
-const ProductList = ( {list} : PropsProductList ) => {
+
+const animationSettings = {
+  initial: { opacity: 0, x: 40 },
+  animate: { opacity: 1, x: 0 },
+  transition: { duration: 0.4 },
+};
+
+const ProductList = ({ list }: PropsProductList) => {
+  const [currentList, setCurrentList] = useState<Array<ProductOptions>>([]);
+
+  useEffect(() => {
+    const isChanged = list.some(
+      (item, index) => item.id !== currentList[index]?.id
+    );
+
+    if (isChanged || list.length === 0) {
+      setCurrentList(list);
+    }
+  }, [list]);
+
   return (
-    <div className="flex items-center flex-wrap mt-2">
-      {list.length > 0 ? list.map((info, index) => (
-        <Product key={index}
-          id={info.id}
-          productName={info.productName}
-          price={info.price}
-          image={info.image}
-        ></Product>
-      )) 
-        : <p className="w-full h-40 flex items-center justify-center text-[#555555]">메뉴 준비중입니다.</p>
-      }
+    <div className="mt-2">
+      {currentList.length > 0 ? (
+        <motion.div
+          className="flex flex-wrap items-center"
+          initial={animationSettings.initial}
+          animate={animationSettings.animate}
+          transition={animationSettings.transition}
+          key={currentList.map((item) => item.id).join("-")}
+        >
+          {currentList.map((info) => (
+            <Product
+              key={info.id}
+              id={info.id}
+              productName={info.productName}
+              price={info.price}
+              image={info.image}
+            ></Product>
+          ))}
+        </motion.div>
+      ) : (
+        <p className="h-24 text-[#555555] text-center py-20">
+          상품 준비중입니다.
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default ProductList;
