d78c200b472e90bea7606e788edcf379e6cffa44
seominki <tjalsrl0668@naver.com>
2024-06-25 11:26:09 +0900

    [Remove] 기획 변경으로 인한 파일 제거

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 's3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/paymint.linq.seoul/**',
-      },
-    ],
-  }
+  images: {}
 };
 
 export default nextConfig;
