203f5b939c38fac18d6b60d61003e853c94c5b6a
seominki <tjalsrl0668@naver.com>
2024-07-01 16:00:09 +0900

    [Feat] 토스페이먼츠 위젯 - 상품명 추가

--- src/components/payment/PaymentWidget.tsx
+++ src/components/payment/PaymentWidget.tsx
@@ -1,61 +1,67 @@
 import { useState, useEffect, useRef } from "react";
-import { fetchPaymentWidget, renderPaymentWidget, handlePaymentRequest } from "@/hooks/paymentRequest";
-import { motion } from 'framer-motion';
+import {
+  fetchPaymentWidget,
+  renderPaymentWidget,
+  handlePaymentRequest,
+} from "@/hooks/paymentRequest";
+import { motion } from "framer-motion";
 import { SlideUp } from "@/utils/animation";
 
 interface PaymentWidgetProps {
   price: number;
-  payRequest: boolean,
+  payRequest: boolean;
+  productName: string;
   handleWidget: () => void;
 }
 
-const PaymentWidget = (({ price, payRequest, handleWidget }: PaymentWidgetProps) => {
-    const [paymentWidget, setPaymentWidget] = useState<any>(null);
-    const paymentMethodsWidgetRef = useRef<HTMLDivElement>(null);
+const PaymentWidget = ({
+  price,
+  payRequest,
+  handleWidget,
+  productName,
+}: PaymentWidgetProps) => {
+  const [paymentWidget, setPaymentWidget] = useState<any>(null);
+  const paymentMethodsWidgetRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-      const init = async () => {
-        const fetch = await fetchPaymentWidget();
-        setPaymentWidget(fetch);
-      };
+  useEffect(() => {
+    const init = async () => {
+      const fetch = await fetchPaymentWidget();
+      setPaymentWidget(fetch);
+    };
 
-      if (payRequest) {
-        init();
-      }
+    if (payRequest) {
+      init();
+    }
+  }, [payRequest]);
 
-      
-    }, [payRequest]);
+  useEffect(() => {
+    if (paymentWidget && price) {
+      renderPaymentWidget(paymentWidget, price, paymentMethodsWidgetRef);
+    }
+  }, [paymentWidget, price]);
 
-    useEffect(() => {
-      if (!paymentWidget || !price) {
-        return;
-      }
-      
-      renderPaymentWidget(paymentWidget, price, paymentMethodsWidgetRef)
-    }, [paymentWidget, price]);
-
-    return (
-      <div className="fixed px-5 left-0 top-0 w-full h-full">
-        <motion.div className="px-5 w-full h-full flex items-center justify-center"
-          onClick={handleWidget}
-          initial={SlideUp.initial}
-          animate={SlideUp.animate}
-          exit={SlideUp.exit}
-          transition={SlideUp.transition}
-        >
-          <section className="w-[640px] rounded-xl overflow-hidden">
-            <div id="payment-widget" />
-            <button
-              onClick={() => handlePaymentRequest(paymentWidget)}
-              className="w-full bg-blue p-4 text-white"
-            >
-              결제하기
-            </button>
-          </section>
-        </motion.div>
-      </div>
-    );
-  }
-);
+  return (
+    <div className="fixed px-5 left-0 top-0 w-full h-full bg-opacity z-10">
+      <motion.div
+        className="px-5 w-full h-full flex items-center justify-center"
+        onClick={handleWidget}
+        initial={SlideUp.initial}
+        animate={SlideUp.animate}
+        exit={SlideUp.exit}
+        transition={SlideUp.transition}
+      >
+        <section className="w-[640px] rounded-xl overflow-hidden">
+          <div id="payment-widget"></div>
+          <button
+            onClick={() => handlePaymentRequest(paymentWidget, productName)}
+            className="w-full bg-blue p-4 text-white"
+          >
+            결제하기
+          </button>
+        </section>
+      </motion.div>
+    </div>
+  );
+};
 
 export default PaymentWidget;
--- src/hooks/paymentRequest.ts
+++ src/hooks/paymentRequest.ts
@@ -1,35 +1,30 @@
-import { loadPaymentWidget } from '@tosspayments/payment-widget-sdk'
-
+import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk";
 
 export async function fetchPaymentWidget() {
-  const clientKey: string = 'test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm'
-  const customerKey: string = 'mZldYje7xPVYoSu1vA_jy'
+  const clientKey: string = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
+  const customerKey: string = "mZldYje7xPVYoSu1vA_jy";
   const paymentWidget = await loadPaymentWidget(clientKey, customerKey);
   return paymentWidget;
 }
 
 export function renderPaymentWidget(widget: any, price: number, ref: any) {
-  const paymentMethodsWidget = widget.renderPaymentMethods(
-    "#payment-widget",
-    { value: price },
-  );
-
-  ref.current = paymentMethodsWidget;
-
+  const paymentMethodsWidget = widget.renderPaymentMethods("#payment-widget", {
+    value: price,
+  });
+  paymentMethodsWidget.on("ready", () => {
+    // 결제 버튼 활성화
+    ref.current = paymentMethodsWidget;
+  });
 }
 
-export async function handlePaymentRequest(widget: any) {
-  try {
-    await widget?.requestPayment({
-      orderId: "V1StGXR8_Z5jdHi6B-myT",
-      orderName: "토스 티셔츠 외 2건",
-      customerName: "김토스",
-      customerEmail: "customer123@gmail.com",
-      customerMobilePhone: "01012341234",
-      successUrl: `${window.location.origin}/success`,
-      failUrl: `${window.location.origin}/fail`,
-    });
-  } catch(e) {
-    console.error(e);
-  }
+export function handlePaymentRequest(widget: any, productName: string) {
+  widget?.requestPayment({
+    orderId: "V1StGXR8_Z5jdHi6B-myT",
+    orderName: productName,
+    customerName: "김토스",
+    customerEmail: "seominki0108@gmail.com",
+    customerMobilePhone: "01012341234",
+    successUrl: `${window.location.origin}/success`,
+    failUrl: `${window.location.origin}/fail`,
+  });
 }
