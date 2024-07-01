47a74f916f2ef8c596d14b267828e15a44e6d7ec
seominki <tjalsrl0668@naver.com>
2024-07-01 15:57:12 +0900

    [Design] 슬라이드업 애니메이션 효과 수정

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,12 +1,14 @@
+import { easeIn, easeInOut, easeOut } from "framer-motion";
+
 export const SlideUp = {
   initial: { y: "100%" },
   animate: { y: 0 },
-  exit: {y: "100%"},
-  transition: { duration: 0.5 },
-}
+  exit: { y: "100%" },
+  transition: { ease: "easeIn", duration: 0.3 },
+};
 
 export const SlideLeftAndOpacity = {
   initial: { opacity: 0, x: 40 },
   animate: { opacity: 1, x: 0 },
   transition: { duration: 0.4 },
-}
+};
