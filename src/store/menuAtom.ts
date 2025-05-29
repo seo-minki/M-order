d26974f496169dba87c24f39cadea596adc02342
seominki <tjalsrl0668@naver.com>
2025-05-29 18:26:16 +0900

    [Fix] API 호출 도메인 절대경로로 변경

--- src/store/menuAtom.ts
+++ src/store/menuAtom.ts
@@ -2,9 +2,7 @@ import {atom} from 'jotai';
 import {fetchMenu} from '@/utils/api';
 import {CategoryOptions} from '@/types/menu';
 
-const menuFetcherAtom = atom(async (): Promise<CategoryOptions[]> => {
+export const menuAtom = atom(async (): Promise<CategoryOptions[]> => {
   const res = await fetchMenu();
   return res.result;
 });
-
-export const menuAtom = atom((get) => get(menuFetcherAtom));
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
 export async function fetchMenu() {
-  const response = await fetch("/api/menu");
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/menu`);
   return response.json();
 }
