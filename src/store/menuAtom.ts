6aaf0194986523f9ac174ea222e231162ce092cc
seominki <tjalsrl0668@naver.com>
2025-05-29 18:16:41 +0900

    [Fix] atom 클라이언트에서 호출되도록 변경

--- src/store/menuAtom.ts
+++ src/store/menuAtom.ts
@@ -2,8 +2,9 @@ import {atom} from 'jotai';
 import {fetchMenu} from '@/utils/api';
 import {CategoryOptions} from '@/types/menu';
 
-
-export const menuAtom = atom(async (): Promise<CategoryOptions[]> => {
+const menuFetcherAtom = atom(async (): Promise<CategoryOptions[]> => {
   const res = await fetchMenu();
   return res.result;
 });
+
+export const menuAtom = atom((get) => get(menuFetcherAtom));
