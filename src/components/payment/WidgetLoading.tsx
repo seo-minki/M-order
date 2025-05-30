3c46d508ca37f7899403711267de1e638486b0dd
seominki <tjalsrl0668@naver.com>
2025-05-30 11:36:38 +0900

    [Feat] 로딩 사이즈 조정

--- src/components/payment/WidgetLoading.tsx
+++ src/components/payment/WidgetLoading.tsx
@@ -4,8 +4,8 @@ import loadingAnimation from 'public/lottie/payment-widget.json';
 
 const WidgetLoading = () => {
   return (
-    <div className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 w-[640px] h-[422px] rounded-xl bg-white">
-      <LottieAnimation animationData={loadingAnimation} loop={true} autoplay={true} className="w-full h-full" />
+    <div className="fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 w-[640px] h-[422px] rounded-xl bg-white flex items-center justify-center">
+      <LottieAnimation animationData={loadingAnimation} loop={true} autoplay={true} className="w-24 h-24" />
     </div>
   );
 };
